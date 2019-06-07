1d62dd1605a6120c095249093a5cb8141c4d6958
Sudipt-panda <44495830+Sudipt-panda@users.noreply.github.com>
2019-06-07 21:05:24 +0200

    Create Assignment_2_output.py
    Output for assignment 2

---
+++ Assignment 2/Assignment_2_output.py
@@ -0,0 +1,326 @@
+Enter name of player 1 :: ram
+Enter name of player 2 :: shyam
+----------------------------
+Name Player :  ram
+Cards are... [14, 4, 6, 2, 12, 8, 16, 10, 18]
+Current point of Player is :: 0
+----------------------------
+Name Player :  shyam
+Cards are... [11, 13, 3, 15, 9, 7, 1, 5, 17]
+Current point of Player is :: 0
+Throw Dice.....
+For Player  ram  Dice Score is  4
+For Player  shyam  Dice Score is  6
+shyam  has to start Game...
+=======Round  Starts=====================================
+----------------------------
+Name Player :  shyam
+Cards are... [11, 13, 3, 15, 9, 7, 1, 5, 17]
+Current point of Player is :: 0
+----------------------------
+Name Player :  ram
+Cards are... [14, 4, 6, 2, 12, 8, 16, 10, 18]
+Current point of Player is :: 0
+=========== shyam 's picked Card=====================
+Details of Character & its Characterstics Strength are ::
+*****Id :12
+*****Name :  Australia
+population ***** 24
+gdp ***** 1323
+area ***** 769
+lexpectancy ***** 84
+---------------
+shyam,pls Enter Character to Challenge(population/gdp/area/lexpectancy) gdp
+shyam ,Do u want to take God Spell Now (press y or n) n
+=========== ram 's picked Card=====================
+Details of Character & its Characterstics Strength are ::
+*****Id :10
+*****Name :  Singapore
+population ***** 5
+gdp ***** 323
+area ***** 7
+lexpectancy ***** 80
+---------------
+=====outdated === {17, 18}
+=======This round won by  shyam ==============
+=======Round  Starts=====================================
+----------------------------
+Name Player :  shyam
+Cards are... [11, 13, 3, 15, 9, 7, 1, 5]
+Current point of Player is :: 1
+----------------------------
+Name Player :  ram
+Cards are... [14, 4, 6, 2, 12, 8, 16, 10]
+Current point of Player is :: 0
+shyam ,Do u want to take Resurrect Spell Now (press y or n) n
+=========== shyam 's picked Card=====================
+Details of Character & its Characterstics Strength are ::
+*****Id :4
+*****Name :  Japan
+population ***** 126
+gdp ***** 4872
+area ***** 37
+lexpectancy ***** 86
+---------------
+shyam,pls Enter Character to Challenge(population/gdp/area/lexpectancy) area
+shyam ,Do u want to take God Spell Now (press y or n) n
+ram ,Do u want to take Resurrect Spell Now (press y or n) n
+=========== ram 's picked Card=====================
+Details of Character & its Characterstics Strength are ::
+*****Id :1
+*****Name :  India
+population ***** 1339
+gdp ***** 2651
+area ***** 3287
+lexpectancy ***** 68
+---------------
+=====outdated === {17, 18, 10, 5}
+=======This round won by  ram ==============
+=======Round  Starts=====================================
+----------------------------
+Name Player :  ram
+Cards are... [14, 4, 6, 2, 12, 8, 16]
+Current point of Player is :: 1
+----------------------------
+Name Player :  shyam
+Cards are... [11, 13, 3, 15, 9, 7, 1]
+Current point of Player is :: 1
+ram ,Do u want to take Resurrect Spell Now (press y or n) n
+=========== ram 's picked Card=====================
+Details of Character & its Characterstics Strength are ::
+*****Id :3
+*****Name :  USA
+population ***** 325
+gdp ***** 19485
+area ***** 9834
+lexpectancy ***** 78
+---------------
+ram,pls Enter Character to Challenge(population/gdp/area/lexpectancy) gdp
+ram ,Do u want to take God Spell Now (press y or n) y
+Enter card no between 0 & 64
+shyam ,Do u want to take Resurrect Spell Now (press y or n) y
+=========== shyam 's picked Card=====================
+Details of Character & its Characterstics Strength are ::
+*****Id :2
+*****Name :  Germany
+population ***** 82
+gdp ***** 3693
+area ***** 35
+lexpectancy ***** 81
+---------------
+=====outdated === {5, 9, 10, 16, 17, 18}
+=======This round won by  ram ==============
+=======Round  Starts=====================================
+----------------------------
+Name Player :  ram
+Cards are... [14, 4, 6, 2, 12, 8]
+Current point of Player is :: 2
+----------------------------
+Name Player :  shyam
+Cards are... [11, 13, 3, 15, 7, 1, 1]
+Current point of Player is :: 1
+ram ,Do u want to take Resurrect Spell Now (press y or n) y
+=========== ram 's picked Card=====================
+Details of Character & its Characterstics Strength are ::
+*****Id :4
+*****Name :  Japan
+population ***** 126
+gdp ***** 4872
+area ***** 37
+lexpectancy ***** 86
+---------------
+ram,pls Enter Character to Challenge(population/gdp/area/lexpectancy) area
+=========== shyam 's picked Card=====================
+Details of Character & its Characterstics Strength are ::
+*****Id :13
+*****Name :  Pakistan
+population ***** 197
+gdp ***** 30
+area ***** 88
+lexpectancy ***** 66
+---------------
+=====outdated === {1, 5, 9, 10, 16, 17, 18}
+=======This round won by  shyam ==============
+=======Round  Starts=====================================
+----------------------------
+Name Player :  shyam
+Cards are... [11, 13, 3, 15, 7, 1]
+Current point of Player is :: 2
+----------------------------
+Name Player :  ram
+Cards are... [14, 4, 6, 2, 12, 8]
+Current point of Player is :: 2
+=========== shyam 's picked Card=====================
+Details of Character & its Characterstics Strength are ::
+*****Id :13
+*****Name :  Pakistan
+population ***** 197
+gdp ***** 30
+area ***** 88
+lexpectancy ***** 66
+---------------
+shyam,pls Enter Character to Challenge(population/gdp/area/lexpectancy) gdp
+shyam ,Do u want to take God Spell Now (press y or n) y
+Enter card no between 0 & 53
+=========== ram 's picked Card=====================
+Details of Character & its Characterstics Strength are ::
+*****Id :18
+*****Name :  Russia
+population ***** 144
+gdp ***** 1578
+area ***** 17123
+lexpectancy ***** 72
+---------------
+=====outdated === {1, 2, 5, 9, 10, 16, 17, 18}
+=======This round won by  ram ==============
+=======Round  Starts=====================================
+----------------------------
+Name Player :  ram
+Cards are... [14, 4, 6, 12, 8]
+Current point of Player is :: 3
+----------------------------
+Name Player :  shyam
+Cards are... [11, 13, 3, 15, 7]
+Current point of Player is :: 2
+=========== ram 's picked Card=====================
+Details of Character & its Characterstics Strength are ::
+*****Id :7
+*****Name :  China
+population ***** 1386
+gdp ***** 12238
+area ***** 9597
+lexpectancy ***** 76
+---------------
+ram,pls Enter Character to Challenge(population/gdp/area/lexpectancy) gdp
+=========== shyam 's picked Card=====================
+Details of Character & its Characterstics Strength are ::
+*****Id :15
+*****Name :  Kenya
+population ***** 49
+gdp ***** 79
+area ***** 58
+lexpectancy ***** 67
+---------------
+=====outdated === {1, 2, 5, 7, 8, 9, 10, 16, 17, 18}
+=======This round won by  ram ==============
+=======Round  Starts=====================================
+----------------------------
+Name Player :  ram
+Cards are... [14, 4, 6, 12]
+Current point of Player is :: 4
+----------------------------
+Name Player :  shyam
+Cards are... [11, 13, 3, 15]
+Current point of Player is :: 2
+=========== ram 's picked Card=====================
+Details of Character & its Characterstics Strength are ::
+*****Id :14
+*****Name :  Bangladesh
+population ***** 164
+gdp ***** 24
+area ***** 14
+lexpectancy ***** 71
+---------------
+ram,pls Enter Character to Challenge(population/gdp/area/lexpectancy) area
+=========== shyam 's picked Card=====================
+Details of Character & its Characterstics Strength are ::
+*****Id :9
+*****Name :  Malaysia
+population ***** 32
+gdp ***** 314
+area ***** 33
+lexpectancy ***** 77
+---------------
+=====outdated === {1, 2, 5, 7, 8, 9, 10, 12, 15, 16, 17, 18}
+=======This round won by  shyam ==============
+=======Round  Starts=====================================
+----------------------------
+Name Player :  shyam
+Cards are... [11, 13, 3]
+Current point of Player is :: 3
+----------------------------
+Name Player :  ram
+Cards are... [14, 4, 6]
+Current point of Player is :: 4
+=========== shyam 's picked Card=====================
+Details of Character & its Characterstics Strength are ::
+*****Id :5
+*****Name :  France
+population ***** 67
+gdp ***** 2583
+area ***** 64
+lexpectancy ***** 82
+---------------
+shyam,pls Enter Character to Challenge(population/gdp/area/lexpectancy) gdp
+=========== ram 's picked Card=====================
+Details of Character & its Characterstics Strength are ::
+*****Id :11
+*****Name :  Brazil
+population ***** 209
+gdp ***** 2054
+area ***** 8516
+lexpectancy ***** 75
+---------------
+=====outdated === {1, 2, 3, 5, 6, 7, 8, 9, 10, 12, 15, 16, 17, 18}
+=======This round won by  shyam ==============
+=======Round  Starts=====================================
+----------------------------
+Name Player :  shyam
+Cards are... [11, 13]
+Current point of Player is :: 4
+----------------------------
+Name Player :  ram
+Cards are... [14, 4]
+Current point of Player is :: 4
+=========== shyam 's picked Card=====================
+Details of Character & its Characterstics Strength are ::
+*****Id :16
+*****Name :  South-Africa
+population ***** 56
+gdp ***** 348
+area ***** 122
+lexpectancy ***** 63
+---------------
+shyam,pls Enter Character to Challenge(population/gdp/area/lexpectancy) gdp
+=========== ram 's picked Card=====================
+Details of Character & its Characterstics Strength are ::
+*****Id :6
+*****Name :  Italy
+population ***** 60
+gdp ***** 1944
+area ***** 30
+lexpectancy ***** 83
+---------------
+=====outdated === {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 15, 16, 17, 18}
+=======This round won by  ram ==============
+=======Round  Starts=====================================
+----------------------------
+Name Player :  ram
+Cards are... [14]
+Current point of Player is :: 5
+----------------------------
+Name Player :  shyam
+Cards are... [11]
+Current point of Player is :: 4
+=========== ram 's picked Card=====================
+Details of Character & its Characterstics Strength are ::
+*****Id :8
+*****Name :  Britain
+population ***** 31
+gdp ***** 2709
+area ***** 25
+lexpectancy ***** 74
+---------------
+ram,pls Enter Character to Challenge(population/gdp/area/lexpectancy) gdp
+=========== shyam 's picked Card=====================
+Details of Character & its Characterstics Strength are ::
+*****Id :17
+*****Name :  Zimbave
+population ***** 16
+gdp ***** 22
+area ***** 39
+lexpectancy ***** 61
+---------------
+=====outdated === {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18}
+=======This round won by  ram ==============
+Congratulations  ram  u have won the Game....
